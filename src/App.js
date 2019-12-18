f30ba93a0643a8e5fc97f56252249d743982e843
Ryan Lesson <ryan.lesson@tekzenit.com>
2019-12-18 10:57:05 -0600

    finish

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { ReactComponent as Logo } from "./logo.svg";
+
+const Star = ({ starId, rating, onMouseEnter, onMouseLeave, onClick }) => {
+  let styleClass = "star-rating-blank";
+  if (rating >= starId) {
+    styleClass = "star-rating-filled";
+  }
+
+  return (
+    <div
+      className="star"
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+      onClick={onClick}
+    >
+      <svg
+        height="55px"
+        width="53px"
+        class={styleClass}
+        viewBox="0 0 25 23"
+        data-rating="1"
+      >
+        <polygon
+          stroke-width="0"
+          points="9.9, 1.1, 3.3, 21.78, 19.8, 8.58, 0, 8.58, 16.5, 21.78"
+        />
+      </svg>
+    </div>
+  );
+};
 
 function App() {
+  const [rating, setRating] = useState(0);
+  const [hoverRating, setHoverRating] = useState(0);
+  const stars = [1, 2, 3, 4, 5];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="header">
+        <h1>Star Rating</h1>
+        <Logo />
+        <div class="logo"></div>
+      </div>
+      <div class="flex-container">
+        {stars.map((star, i) => (
+          <Star
+            key={i}
+            starId={i}
+            rating={hoverRating || rating}
+            onMouseEnter={() => setHoverRating(i)}
+            onMouseLeave={() => setHoverRating(0)}
+            onClick={() => setRating(i)}
+          />
+        ))}
+      </div>
     </div>
   );
 }
